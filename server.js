17894048267bd7aec6d545a1b2e2a7c1f79f087b
Peter Ajiboye <deuceclaver@gmail.com>
2026-04-23 13:50:12 +0100

    corrections

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.get('/api/profiles/search', async (req, res) => {
     }
 });
 
-app.get('/api/profiles/:id', async (req, res) => {
+app.get('/api/profiles/:id([0-9a-fA-F-]{36})', async (req, res) => {
     if (req.params.id === 'search') return next();
     try {
         const profile = await Profile.findByPk(req.params.id);
